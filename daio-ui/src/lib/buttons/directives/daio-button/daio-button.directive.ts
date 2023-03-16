eb28bda78076e8ea1e767bb2923a5934e2fbe278
Damian <dkowalski.1997@gmail.com>
2023-03-16 01:30:55 +0100

    prettier

--- daio-ui/src/lib/buttons/directives/daio-button/daio-button.directive.ts
+++ daio-ui/src/lib/buttons/directives/daio-button/daio-button.directive.ts
@@ -1,38 +1,48 @@
-import { AfterViewInit, Directive, ElementRef, HostBinding, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  Directive,
+  ElementRef,
+  HostBinding,
+  Input,
+} from '@angular/core';
 import { DaioButtonRendererService } from '../../services/daio-button-renderer.service';
 
 @Directive({
-    standalone: true,
-    selector: '[daioButton]',
-    providers: [DaioButtonRendererService],
+  standalone: true,
+  selector: '[daioButton]',
+  providers: [DaioButtonRendererService],
 })
 export class DaioButtonDirective implements AfterViewInit {
-    constructor(
-        private elementRef: ElementRef, 
-        private renderer: DaioButtonRendererService
-    ) {
-        this.renderer.setHTMLElements(this.elementRef);
-    }
+  constructor(
+    private elementRef: ElementRef,
+    private renderer: DaioButtonRendererService
+  ) {
+    this.renderer.setHTMLElements(this.elementRef);
+  }
 
-    @HostBinding('class') buttonClass = 'daio-button';
+  @HostBinding('class') buttonClass = 'daio-button';
 
-    @Input() set disabled(isDisabled: boolean) {
-        isDisabled ? this.addClass('daio-button--disabled') : this.removeClass('daio-button--disabled');
-    }
+  @Input() set disabled(isDisabled: boolean) {
+    isDisabled
+      ? this.addClass('daio-button--disabled')
+      : this.removeClass('daio-button--disabled');
+  }
 
-    @Input() set loading(isLoading: boolean) {
-        isLoading ? this.addClass('daio-button--loading') : this.removeClass('daio-button--loading');
-    }
+  @Input() set loading(isLoading: boolean) {
+    isLoading
+      ? this.addClass('daio-button--loading')
+      : this.removeClass('daio-button--loading');
+  }
 
-    public ngAfterViewInit(): void {
-        this.renderer.addWrapper(this.elementRef);
-    }
+  public ngAfterViewInit(): void {
+    this.renderer.addWrapper(this.elementRef);
+  }
 
-    private addClass(name: string): void {
-        this.renderer.addClass(name);
-    }
+  private addClass(name: string): void {
+    this.renderer.addClass(name);
+  }
 
-    private removeClass(name: string): void {
-        this.renderer.removeClass(name);
-    }
+  private removeClass(name: string): void {
+    this.renderer.removeClass(name);
+  }
 }

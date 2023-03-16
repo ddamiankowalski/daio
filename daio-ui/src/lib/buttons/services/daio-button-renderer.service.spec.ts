567b03790cb0f9625784cb848fc77955ec0a2fb9
Damian Kowalski <dkowalski.1997@gmail.com>
2023-03-16 21:13:09 +0100

    added test for daiobuttonrenderer

--- daio-ui/src/lib/buttons/services/daio-button-renderer.service.spec.ts
+++ daio-ui/src/lib/buttons/services/daio-button-renderer.service.spec.ts
@@ -2,15 +2,27 @@ import { ElementRef, Renderer2 } from "@angular/core";
 import { TestBed } from "@angular/core/testing";
 import { DaioButtonRendererService } from "./daio-button-renderer.service";
 
+
 describe('DaioButtonRendererService', () => {
     let buttonRendererService: DaioButtonRendererService;
     let expectedElementRef: ElementRef;
+
+    const addClassMock = jest.fn();
+    const removeClassMock = jest.fn();
+
+    const renderer2Mock: Partial<Renderer2> = {
+        addClass: addClassMock,
+        removeClass: removeClassMock
+    }
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             providers: [
                 DaioButtonRendererService, 
-                Renderer2,
+                {
+                    provide: Renderer2,
+                    useValue: renderer2Mock
+                },
                 {
                     provide: ElementRef,
                     useValue: new ElementRef(document.createElement('button'))
@@ -20,14 +32,21 @@ describe('DaioButtonRendererService', () => {
 
        buttonRendererService = TestBed.inject(DaioButtonRendererService);
        expectedElementRef = new ElementRef(document.createElement('button'));
+       buttonRendererService.setHTMLElements();
     });
 
     it('initializes successfully', () => {
         expect(buttonRendererService).toBeDefined();
     });
 
-    it('sets the html element instance to the service buttonhtmlelement', () => {
-        buttonRendererService.setHTMLElements();
+    it('sets the html button instance to the service buttonhtmlelement', () => {
         expect(buttonRendererService['buttonHTMLElement']).toEqual(expectedElementRef.nativeElement);
+    });
+
+    it('adds the class to the host button element', () => {
+        buttonRendererService.addClass('testclass');
+        expect(renderer2Mock.addClass).toHaveBeenCalled();
+        expect(addClassMock.mock.calls[0][0]).toEqual(expectedElementRef.nativeElement);
+        expect(addClassMock.mock.calls[0][1]).toBe('testclass');
     });
 });

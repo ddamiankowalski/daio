fe776cb9f0a53ed798bcc9e3793d0ac0c8458ef4
Damian <dkowalski.1997@gmail.com>
2023-04-01 00:42:35 +0200

    working sidenav css

--- daio-ui/src/lib/sidenav/components/sidenav/daio-sidenav.component.ts
+++ daio-ui/src/lib/sidenav/components/sidenav/daio-sidenav.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, HostBinding, ViewEncapsulation } from "@angular/core";
 import { DaioSidenavHamburgerComponent } from "../sidenav-hamburger/daio-sidenav-hamburger.component";
+import { DaioRendererService } from "../../../common/services/daio-renderer.service";
 
 @Component({
     standalone: true,
@@ -7,17 +8,26 @@ import { DaioSidenavHamburgerComponent } from "../sidenav-hamburger/daio-sidenav
     templateUrl: './daio-sidenav.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
     encapsulation: ViewEncapsulation.None,
-    imports: [
-        DaioSidenavHamburgerComponent
-    ]
+    imports: [DaioSidenavHamburgerComponent],
+    providers: [DaioRendererService]
 })
 export class DaioSidenavComponent {
     @HostBinding('class') sidenavClass = 'daio-sidenav';
 
-    isExpanded = false;
+    constructor(private renderer: DaioRendererService) {}
+
+    isExpanded = true;
 
     handleHamburgerClicked(isExpanded: boolean): void {
         this.isExpanded = isExpanded;
-        console.log(this.isExpanded);
+        this.isExpanded ? this.addExpandedClass() : this.removeExpandedClass();
+    }
+
+    private addExpandedClass(): void {
+        this.renderer.addClass('daio-sidenav--expanded');
+    }
+
+    private removeExpandedClass(): void {
+        this.renderer.removeClass('daio-sidenav--expanded');
     }
 }

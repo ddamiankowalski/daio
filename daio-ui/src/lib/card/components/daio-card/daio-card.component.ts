3ad7f844d33fdbc2186fc8144f1616c203b03e2f
Damian <dkowalski.1997@gmail.com>
2023-04-05 19:27:44 +0200

    added cardtitle/cardavatar

--- daio-ui/src/lib/card/components/daio-card/daio-card.component.ts
+++ daio-ui/src/lib/card/components/daio-card/daio-card.component.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from "@angular/common";
-import { AfterViewInit, ContentChildren, HostBinding, QueryList, Type } from "@angular/core";
+import { HostBinding } from "@angular/core";
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from "@angular/core";
-import { DaioCardHeaderComponent } from "../daio-card-header/daio-card-header.component";
 
 @Component({
     standalone: true,
@@ -11,18 +10,6 @@ import { DaioCardHeaderComponent } from "../daio-card-header/daio-card-header.co
     encapsulation: ViewEncapsulation.None,
     imports: [CommonModule]
 })
-export class DaioCardComponent implements AfterViewInit {
-    @ContentChildren(DaioCardHeaderComponent) set headerComponent(header: QueryList<DaioCardHeaderComponent>) {
-        header.forEach((x: DaioCardHeaderComponent) => {
-            this.header = x.constructor as any;
-        });
-    }
-
-    header!: Type<DaioCardHeaderComponent>;
-
+export class DaioCardComponent {
     @HostBinding('class') cardClass = 'daio-card';
-
-    ngAfterViewInit(): void {
-        console.log(this.header)
-    }
 }

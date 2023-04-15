6e06e19c06164d14133b3c42f1d08e253ccbd987
Damian <dkowalski.1997@gmail.com>
2023-04-15 18:46:24 +0200

    added animation on widget

--- daio-ui/src/lib/sidenav/components/daio-sidenav-widget/daio-sidenav-widget.component.ts
+++ daio-ui/src/lib/sidenav/components/daio-sidenav-widget/daio-sidenav-widget.component.ts
@@ -1,8 +1,10 @@
-import { ChangeDetectionStrategy, Component, HostBinding, Input, ViewEncapsulation } from "@angular/core";
+import { ChangeDetectionStrategy, Component, HostBinding, Input, OnDestroy, OnInit, ViewEncapsulation } from "@angular/core";
 import { DaioWidgetComponent } from "../../../widget-menu/components/daio-widget/daio-widget.component";
 import { IDaioWidget } from "../../../widget-menu/interfaces/daio-widget.interface";
 import { NgFor } from '@angular/common';
 import { DaioWidgetNewsComponent } from "../../../widget-menu/components/daio-widget-news/daio-widget-news.component";
+import { DaioSidenavService } from "../../services/daio-sidenav.service";
+import { Subscription } from "rxjs";
 
 @Component({
     standalone: true,
@@ -16,8 +18,25 @@ import { DaioWidgetNewsComponent } from "../../../widget-menu/components/daio-wi
         DaioWidgetNewsComponent
     ]
 })
-export class DaioSidenavWidgetComponent {
+export class DaioSidenavWidgetComponent implements OnInit, OnDestroy {
     @HostBinding('class') widgetMenuClass = 'daio-sidenav-widget';
+    @HostBinding('class.daio-sidenav-widget--hidden') widgetHidden?: boolean;
 
     @Input() widgets?: IDaioWidget[];
+
+    private _subscription?: Subscription;
+
+    constructor(private sidenav: DaioSidenavService) {}
+
+    ngOnInit(): void {
+        this._subscription = this.sidenav.isExpanded$.subscribe(isExpanded => this.toggleWidget(isExpanded));
+    }
+
+    ngOnDestroy(): void {
+        this._subscription?.unsubscribe();
+    }
+
+    private toggleWidget(isExpanded: boolean) {
+        this.widgetHidden = isExpanded;
+    }
 }

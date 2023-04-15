3bcf7b2673b8b4076733dabcb795de28546e16d6
Damian <dkowalski.1997@gmail.com>
2023-04-15 16:27:44 +0200

    added dashboard changes

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -3,8 +3,8 @@ import { RouterModule } from "@angular/router";
 import { dashboardRoutes } from "./routing/dashboard.routes";
 import { DaioDashboardComponent } from "./components/daio-dashboard/daio-dashboard.component";
 import { DaioGlobalHeaderComponent, DaioSidenavComponent, DaioSidenavWidgetComponent } from "daio-ui";
-import { DaioWidgetGradientComponent } from "daio-ui/src/lib/widget-menu/components/daio-widget-gradient/daio-widget-gradient.component";
 import { CommonModule } from "@angular/common";
+import { DaioWidgetNewsComponent } from "daio-ui/src/lib/widget-menu/components/daio-widget-news/daio-widget-news.component";
 
 @NgModule({
     declarations: [
@@ -16,7 +16,7 @@ import { CommonModule } from "@angular/common";
         DaioSidenavComponent,
         DaioSidenavWidgetComponent,
         DaioGlobalHeaderComponent,
-        DaioWidgetGradientComponent
+        DaioWidgetNewsComponent
     ],
     exports: []
 })
